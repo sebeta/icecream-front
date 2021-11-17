8ef6cb611541decf1670776f73cd0059eed5100e
Sebastian <sebeta057@gmail.com>
2021-11-17 18:30:55 -0500

    Forth commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react"
 import { Container } from "react-bootstrap"
 import IcecreamList from "./components/IcecreamList"
-import { getIcecreamAxios, getIcecreamFetch } from "./api/icecream"
-import { API_URL } from "./constants"
+import { getIcecreamFetch } from "./api/icecream"
+// import { API_URL } from "./constants"
 import "bootstrap/dist/css/bootstrap.min.css"
-import axios from "axios"
+// import axios from "axios"
 import "./App.css"
 
 function App() {
   const [icecreams, setIcecreams] = useState([])
 
-  useEffect(async () => {
-    const data = await getIcecreamFetch()
-    // const data = await getIcecreamAxios()
-    setIcecreams(data)
+  useEffect(() => {
+    async function fetch() {
+      const data = await getIcecreamFetch()
+      setIcecreams(data)
+    }
+    fetch()
   }, [])
 
   return (
